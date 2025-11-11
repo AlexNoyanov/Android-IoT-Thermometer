170175cd84426588ebe2e467aaef1adbf8fb8df2
Alex Noyanov <alex.noyanov@gmail.com>
2025-11-11 21:34:04 +0300

    Created Thermometer Basic App template

---
+++ app/src/main/java/com/noyanov/thermometer/MainActivity.kt
@@ -0,0 +1,212 @@
+package com.noyanov.thermometer
+
+import android.Manifest
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Thermostat
+import androidx.compose.material.icons.filled.WaterDrop
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.RoundRect
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.noyanov.thermometer.ui.theme.ThermometerTheme
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.viewmodel.observe
+import kotlin.getValue
+
+class MainActivity : ComponentActivity() {
+    private val viewModel : MainViewModel by viewModels()
+    // 1. Define the launcher as a property of the Activity
+    private val requestPermissionLauncher: ActivityResultLauncher<String> =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your app.
+                //viewModel.sendIntent(MainViewIntent.PermissionGranted)
+                handleSideEffect(MainViewSideEffect.ShowToast("Permission granted"))
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // feature requires a permission that the user has denied.
+                //viewModel.sendIntent(MainViewIntent.PermissionDenied)
+                handleSideEffect(MainViewSideEffect.ShowToast("Permission denied"))
+            }
+        }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 2. Launch the permission request from onCreate
+        requestPermissionLauncher.launch(Manifest.permission.INTERNET)
+
+        // 2. Collect side effects
+        viewModel.observe(
+            lifecycleOwner = this,
+            sideEffect = ::handleSideEffect
+        )
+
+        setContent {
+            ThermometerTheme {
+                MainScreen(viewModel = viewModel)
+            }
+        }
+    }
+
+    private fun handleSideEffect(sideEffect: MainViewSideEffect) {
+        when (sideEffect) {
+            is MainViewSideEffect.ShowToast -> {
+                Toast.makeText(this,
+                    sideEffect.message,
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(viewModel: MainViewModel) {
+    // 1. Collect state from the ViewModel
+    //val state by viewModel.collectAsState()
+    val state = viewModel.collectAsState().value
+    //val context = LocalContext.current
+    //Greeting(state.name, viewModel)
+    // 2. Use LaunchedEffect to run code once when the composable starts
+    LaunchedEffect(Unit) {
+        // This block will run only once
+        // Perfect for telling the ViewModel to load initial data
+        viewModel.sendIntent(MainViewIntent.ConnectToServer)
+    }
+
+    val lifecycleOwner = LocalLifecycleOwner.current // 👈 Get the lifecycle owner
+    // --- DisposableEffect for setup and teardown (cleanup) logic ---
+    DisposableEffect(lifecycleOwner) {
+        // The onDispose block runs when the composable leaves the composition (cleanup)
+        onDispose {
+            viewModel.sendIntent(MainViewIntent.DisconnectServer)
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = { Text("Thermometer") })
+        }
+    ) { innerPadding ->
+        InfoCard(
+            modifier = Modifier.padding(innerPadding),
+            state = state,
+        )
+    }
+}
+
+@Composable
+fun InfoCard(modifier: Modifier = Modifier,
+            state: MainViewState) {
+    Card(modifier = modifier.padding(16.dp),
+        border = BorderStroke(width = 1.dp, color = MaterialTheme.colorScheme.primary)
+        ) {
+        Indicator(imageVector = Icons.Default.Thermostat,
+            contentDescription = "Thermostat",
+            text = state.temperatureStr,
+            onClick = {}
+            )
+        Indicator(imageVector = Icons.Default.WaterDrop,
+            contentDescription = "Humidity",
+            text = state.humidityStr,
+            onClick = {}
+        )
+    }
+}
+
+@Composable
+fun Indicator(imageVector: ImageVector,
+              contentDescription: String,
+              text: String,
+              onClick: ()->Unit) {
+    OutlinedButton(modifier = Modifier
+        .fillMaxWidth()
+        .padding(start = 30.dp, end = 30.dp, top = 10.dp, bottom = 10.dp),
+        //shape = RoundedCornerShape(30.dp),
+        onClick = onClick) {
+        Icon(imageVector = imageVector,
+            contentDescription = contentDescription)
+
+        Text(text)
+    }
+}
+
+
+@Composable
+fun Greeting(name: String,
+             viewModel: MainViewModel,
+             modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+        Button(onClick = { viewModel.sendIntent(MainViewIntent.UpdateName) }) {
+            Text("Update Name")
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    ThermometerTheme {
+        MainScreen(MainViewModel())
+    }
+}
+
+@Preview(
+    showBackground = true
+)
+@Composable
+fun IndicatorPreview() {
+    ThermometerTheme {
+        Indicator(imageVector = Icons.Default.Thermostat,
+            contentDescription = "Thermostat",
+            text = "25.0\u00B0",
+            onClick = {})
+    }
+}
+
+@Preview
+@Composable
+fun InfoCardPreview() {
+    InfoCard(state = MainViewState(temperatureStr = "25.0\u00B0",
+        humidityStr = "50%"))
+}
---
+++ app/src/main/java/com/noyanov/thermometer/MainViewModel.kt
@@ -0,0 +1,116 @@
+package com.noyanov.thermometer
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.viewmodel.container
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.io.path.inputStream
+import kotlin.io.path.readText
+
+data class MainViewState(
+    val name: String = "Android",
+    val temperatureStr: String = "0.0\u00B0",
+    val humidityStr: String = "0.0%",
+    val pressureStr: String = "0.0 mm"
+)
+
+sealed class MainViewSideEffect {
+    data class ShowToast(val message: String) : MainViewSideEffect()
+}
+
+sealed class MainViewIntent {
+    object UpdateName : MainViewIntent()
+    object ConnectToServer : MainViewIntent()
+    object DisconnectServer : MainViewIntent()
+}
+
+
+class MainViewModel :
+    ContainerHost<MainViewState, MainViewSideEffect>, ViewModel() {
+
+    // Include `orbit-viewmodel` for the factory function
+    override val container = container<MainViewState,MainViewSideEffect>(MainViewState())
+
+    fun sendIntent(intent:MainViewIntent) {
+        when (intent) {
+            MainViewIntent.UpdateName -> updateName()
+            MainViewIntent.ConnectToServer -> connectToServer()
+            MainViewIntent.DisconnectServer -> disconnectServer()
+        }
+    }
+
+    private fun updateName() = intent {
+        reduce { state.copy(name = "Android Updated") }
+        postSideEffect(MainViewSideEffect.ShowToast("Name updated"))
+    }
+
+    private fun connectToServer() = intent {
+        reduce { state.copy(temperatureStr = "",
+            humidityStr = "",
+            pressureStr = "")
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                // Replace with your actual API endpoint
+                val url =
+                    URL("https://api.jsonbin.io/v3/b/668b32e0e41b4d34e40e2b48") // Using a sample JSON endpoint
+                val urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.requestMethod = "GET"
+                // urlConnection.setRequestProperty("X-Master-Key", "YOUR_API_KEY") // Add headers if needed
+
+                val responseCode = urlConnection.responseCode
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    val reader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val response = reader.readText()
+                    reader.close()
+
+                    // Parse the JSON response
+                    val jsonResponse = JSONObject(response).getJSONObject("record")
+                    val temp = jsonResponse.getDouble("temperature")
+                    val humidity = jsonResponse.getDouble("humidity")
+                    val pressure = jsonResponse.getDouble("pressure")
+
+                    // Update UI state on the main thread
+                    withContext(Dispatchers.Main) {
+                        reduce {
+                            state.copy(
+                                temperatureStr = "%.1f\u00B0".format(temp),
+                                humidityStr = "%.1f%%".format(humidity),
+                                pressureStr = "%.1f mm".format(pressure)
+                            )
+                        }
+                    }
+                } else {
+                    // Handle HTTP errors
+                    withContext(Dispatchers.Main) {
+                        postSideEffect(MainViewSideEffect.ShowToast("Error: $responseCode"))
+                    }
+                }
+            } catch (e: Exception) {
+                // Handle exceptions like no internet connection
+                withContext(Dispatchers.Main) {
+                    postSideEffect(MainViewSideEffect.ShowToast("Error: ${e.message}"))
+                }
+            }
+        }
+    }
+
+    private fun disconnectServer() = intent {
+        reduce {
+            state.copy(
+                temperatureStr = "0.0\u00B0",
+                humidityStr = "0.0%",
+                pressureStr = "0.0 mm"
+            )
+        }
+        postSideEffect(MainViewSideEffect.ShowToast("Disconnected"))
+    }
+}
